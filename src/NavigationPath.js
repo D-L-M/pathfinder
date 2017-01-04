437d14306801966713f536100ba2e703a6c4e8b6
David Maidment <david@dmaidment.co.uk>
2017-01-04 19:42:36 +0000

    Added stub method for calculating a path

--- src/NavigationPath.js
+++ src/NavigationPath.js
@@ -14,6 +14,24 @@ class NavigationPath
         this.grid = grid;
         this.from = from;
         this.to   = to;
+        this.path = [];
+
+        this._calculatePath();
+
+    }
+
+
+    /**
+     * Calculate the quickest path between the start and end points
+     * @throws exception if it is not possible to plot a path
+     */
+    _calculatePath()
+    {
+
+        if (false) // @todo Add check for impossible path
+        {
+            throw 'No path exists between the requested start and end point';
+        }
 
     }
 
